8ec5e425f588ac1c7516ad73bed892bb50b043ae
noahkise <noah@kisefamily.com>
2024-01-23 11:10:16 -0800

    make progress

--- CarDrop.Solution/CarDrop.Tests/ModelTests/VehicleTests.cs
+++ CarDrop.Solution/CarDrop.Tests/ModelTests/VehicleTests.cs
@@ -1,16 +1,58 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CarDrop.Models;
 
 namespace CarDrop.Tests
 {
   [TestClass]
-  public class CarTests
+  public class VehicleTests
   {
     [TestMethod]
-    public void CarConstructor_CreateAnInstancesOfCar_Car()
+    public void VehicleConstructor_CreateAnInstancesOfVehicle_Vehicle()
     {
-      Car alfaromeo = new()
-      Assert.AreEqual(typeof(alfaromeo), Car.GetType())
+      Vehicle alfaromeo = new("Spyder", 168, 64, 50, 2464);
+      Assert.AreEqual(typeof(Vehicle), alfaromeo.GetType());
+    }
+    [TestMethod]
+    public void VehicleConstructor_GetProperties_IntString()
+    {
+      string name = "Spyder";
+      int length = 168;
+      int width = 64;
+      int height = 50;
+      int weight = 2464;
+      Vehicle alfaromeo = new(name, length, width, height, weight);
+      Assert.AreEqual(alfaromeo.Name, name);
+      Assert.AreEqual(alfaromeo.Length, length);
+      Assert.AreEqual(alfaromeo.Width, width);
+      Assert.AreEqual(alfaromeo.Height, height);
+      Assert.AreEqual(alfaromeo.Weight, weight);
+
+    }
+    [TestMethod]
+    public void VehicleConstructor_SetProperties()
+    {
+      string name = "Spyder";
+      int length = 168;
+      int width = 64;
+      int height = 50;
+      int weight = 2464;
+      Vehicle alfaromeo = new("Toyota", 10, 10, 10, 10);
+      alfaromeo.Name = name;
+      alfaromeo.Length = length;
+      alfaromeo.Width = width;
+      alfaromeo.Height = height;
+      alfaromeo.Weight = weight;
+      Assert.AreEqual(alfaromeo.Name, name);
+      Assert.AreEqual(alfaromeo.Length, length);
+      Assert.AreEqual(alfaromeo.Width, width);
+      Assert.AreEqual(alfaromeo.Height, height);
+      Assert.AreEqual(alfaromeo.Weight, weight);
+    }
+    [TestMethod]
+    public void VehicleConstructor_GetVolume()
+    {
+      Vehicle alfaromeo = new("Spyder", 168, 64, 50, 2464);
+      int expected = 537600;
+      Assert.AreEqual(alfaromeo.GetVolume(), expected);
     }
   }
 }
--- CarDrop.Solution/CarDrop/Models/Vehicle.cs
+++ CarDrop.Solution/CarDrop/Models/Vehicle.cs
@@ -1,7 +1,30 @@
 namespace CarDrop.Models
 {
-  public abstract class Vehicle
+  public class Vehicle
   {
-    
+    public string Name { get; set; }
+    public  int Length { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int Weight { get; set; }
+    public Vehicle(string name, int length, int width, int height, int weight)
+    {
+      Name = name;
+      Length = length;
+      Width = width;
+      Height = height;
+      Weight = weight;
+    }
+    public int GetVolume()
+    {
+      return Length * Width * Height;
+    }
+
+    public double CalcShippingCost()
+    {
+      double density = (double) Weight / GetVolume();
+      return ((double) GetVolume() * 10.99)  * ((double) 100 - density)/100;
+    }
+
   }
 }
